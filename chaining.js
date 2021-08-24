364d6b07396b15fc708dda283a357e7d3af36994
unknown <ebteshamhossain98@gmail.com>
2021-08-25 01:59:02 +0600

    done1

--- chaining.js
+++ chaining.js
@@ -1,1 +1,12 @@
+// declare variable based on the name of an object property
 const myObject = { x: 2, y: 50, z: 600, a: 25, b: 60 };
+const { x, b } = myObject;
+console.log('myObject.p', myObject ? .p ? .q);
+
+// destructuring array
+const [p, q] = [45, 60, 91];
+// console.log(p, q);
+
+const [best, faltu] = [45, 60];
+// console.log(best, faltu);
+const {} = { sky: "blue", soil: "matti", color: "red", money: 600 };
